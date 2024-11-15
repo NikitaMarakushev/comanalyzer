1b8547c2530a4ac16554c7d5b70e421a0785a636
NikitaMarakushev <nikitamarakushev@yandex.ru>
2024-11-15 04:57:53 +0300

    Updated DESCRIPTION.md, pom.xml and Main class

--- src/main/java/nmarakushev/projects/Main.java
+++ src/main/java/nmarakushev/projects/Main.java
@@ -1,17 +1,31 @@
 package nmarakushev.projects;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String pathToSentenceModel = "models\\opennlp-ru-ud-gsd-sentence-1.1-2.4.0.bin";
+        String pathToTokensModel = "models\\opennlp-ru-ud-gsd-tokens-1.1-2.4.0.bin";
+        System.out.println(System.getProperty("user.dir") + "\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        File modelFile = new File(pathToSentenceModel);
+        System.out.println(modelFile.lastModified());
+
+        try (InputStream modelIn = new FileInputStream(modelFile)) {
+            SentenceModel model = new SentenceModel(modelIn);
+            SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
+            String[] sentences = sentenceDetector.sentDetect(" First sentence. Second sentence. ");
+            for (String var : sentences) {
+                System.out.println(var);
+            }
+        } catch (Exception e) {
+            System.out.println("Модель не открылась \n");
+            System.out.println(e.getMessage());
         }
     }
 }
