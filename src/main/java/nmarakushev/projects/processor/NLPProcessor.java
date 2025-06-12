e2fb23baa7f546557bbe93f723c217abd0059959
«NikitaMarakushev» <nikitamarakushev@yandex.ru>
2025-06-12 11:06:13 +0300

    remove unused import

--- src/main/java/nmarakushev/projects/processor/NLPProcessor.java
+++ src/main/java/nmarakushev/projects/processor/NLPProcessor.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import opennlp.tools.stemmer.PorterStemmer;
